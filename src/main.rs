9d2d8b31829f1983ec10f9ae1a6588b523317d1c
Grigory Avdyushin <avdyushin.g@gmail.com>
2018-10-26 16:45:06 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -15,41 +15,59 @@ use std::env;
 
 const DEFAULT_URL: &'static str = "postgres://docker:docker@localhost:5432/bibles";
 
-fn parse_query(query: &str) -> Option<String> {
-    use std::collections::HashMap;
-
-    let args = url::form_urlencoded::parse(&query.as_bytes())
-        .into_owned()
-        .collect::<HashMap<String, String>>();
-
-    args.get("q").map(|v| v.to_string())
-}
-
-type BoxFut = Box<Future<Item = Response<Body>, Error = hyper::Error> + Send>;
-
-fn echo(req: Request<Body>) -> BoxFut {
-    let mut response = Response::new(Body::empty());
-
-    match (req.method(), req.uri().path()) {
-        (&Method::GET, "/") => match req.uri().query() {
-            Some(query) => match parse_query(query) {
-                Some(query) => *response.body_mut() = Body::from(format!("ask? {:?}", query)),
-                None => *response.body_mut() = Body::from("Boo"),
-            },
-            None => *response.body_mut() = Body::from("Foo"),
-        },
-        (&Method::POST, "/echo") => {
-            *response.body_mut() = req.into_body();
-        }
-        _ => {
-            *response.status_mut() = StatusCode::NOT_FOUND;
-        }
-    };
-
-    Box::new(future::ok(response))
-}
-
 struct SearchService;
+
+impl SearchService {
+    fn parse_query(&self, query: &str) -> Option<String> {
+        use std::collections::HashMap;
+
+        let args = url::form_urlencoded::parse(&query.as_bytes())
+            .into_owned()
+            .collect::<HashMap<String, String>>();
+
+        args.get("q").map(|v| v.to_string())
+    }
+
+    fn connect_db(&self) -> Option<Connection> {
+        let url = env::var("DATABASE_URL").unwrap_or(String::from(DEFAULT_URL));
+
+        println!("Connecting: {}", &url);
+        match Connection::connect(url, TlsMode::None) {
+            Ok(connection) => {
+                println!("Success");
+                Some(connection)
+            },
+            Err(error) => {
+                println!("Error: {}", error);
+                None
+            },
+        }
+    }
+
+    fn fetch_results(&self, db: Connection, query: Option<String>) -> String {
+
+        let books = db
+            .query("SELECT id, book, alt, abbr FROM rst_bible_books", &[])
+            .unwrap();
+
+        for row in &books {
+            let book = Book {
+                id: row.get(0),
+                book: row.get(1),
+                alt: row.get(2),
+                abbr: row.get(3),
+            };
+            println!("{:?}", book);
+        }
+
+        String::from("OK")
+    }
+
+    // Verse Of the Day
+    fn vod_response(&self) -> Body {
+        Body::from("Gen 1:1")
+    }
+}
 
 impl NewService for SearchService {
     type ReqBody = Body;
@@ -71,35 +89,50 @@ impl Service for SearchService {
     type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error> + Send>;
 
     fn call(&mut self, request: Request<Self::ReqBody>) -> Self::Future {
-        println!("Get request: {:?}", request);
-        echo(request)
+
+        println!("Got request: {:?}", request);
+
+        let db = match self.connect_db() {
+            Some(db) => Some(db),
+            None => {
+                println!("Error getting DB connection");
+                None
+            },
+        };
+
+        let mut response = Response::new(Body::empty());
+
+        match (request.method(), request.uri().path()) {
+            (&Method::GET, "/") => match request.uri().query() {
+                Some(query) => match self.parse_query(query) {
+                    Some(query) => *response.body_mut() = Body::from(format!("ask? {:?}", query)),
+                    None => *response.body_mut() = self.vod_response(),
+                },
+                None => *response.body_mut() = self.vod_response(),
+            },
+            (&Method::POST, "/") => {
+                //if request.body() == Body::empty() {
+                //    *response.body_mut() = self.vod_response();
+                //} else {
+                    *response.body_mut() = request.into_body();
+                //}
+            }
+            _ => {
+                *response.status_mut() = StatusCode::NOT_FOUND;
+            }
+        };
+
+        Box::new(future::ok(response))
     }
 }
 
 fn main() {
-    let addr = "127.0.0.1:8080".parse().unwrap();
 
+    let addr = "127.0.0.1:8080".parse().unwrap();
     let server = Server::bind(&addr)
         .serve(SearchService)
         .map_err(|e| eprintln!("server error: {}", e));
 
     println!("Listening {}", addr);
     hyper::rt::run(server);
-
-    let url = env::var("DATABASE_URL").unwrap_or(String::from(DEFAULT_URL));
-    println!("Connecting: {}", &url);
-
-    let conn = Connection::connect(url, TlsMode::None).unwrap();
-    let books = conn
-        .query("SELECT id, book, alt, abbr FROM rst_bible_books", &[])
-        .unwrap();
-    for row in &books {
-        let book = Book {
-            id: row.get(0),
-            book: row.get(1),
-            alt: row.get(2),
-            abbr: row.get(3),
-        };
-        println!("{:?}", book);
-    }
 }
