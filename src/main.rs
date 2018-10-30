e61008f254006ca0d349b8947a9dd14e779e6b13
Grigory Avdyushin <avdyushin.g@gmail.com>
2018-10-30 14:57:06 +0100

    Error refactoring

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,27 @@ extern crate url;
 
 mod models;
 use futures::future::{Future, FutureResult};
+use futures::stream::Stream;
 use hyper::body::Payload;
 use hyper::service::{NewService, Service};
-use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
+use hyper::{Body, Chunk, Method, Request, Response, Server, StatusCode};
 use models::*;
 use postgres::{Connection, TlsMode};
 use std::env;
+use std::fmt;
 
 const DEFAULT_URL: &'static str = "postgres://docker:docker@localhost:5432/bibles";
+
+#[derive(Debug)]
+enum ServiceError {
+    NoInput
+}
+impl std::error::Error for ServiceError {}
+impl fmt::Display for ServiceError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Error")
+    }
+}
 
 struct SearchService;
 
@@ -52,13 +65,14 @@ impl SearchService {
         String::from("OK")
     }
 
-    // Verse Of the Day
-    fn vod_response(&self) -> Body {
-        Body::from("Gen 1:1")
-    }
 }
 
-fn parse_query(query: Option<&str>) -> FutureResult<String, Error> {
+// Verse Of the Day
+fn vod_response_body() -> Body {
+    Body::from("Gen 1:1")
+}
+
+fn parse_query(query: Option<&str>) -> FutureResult<String, ServiceError> {
     use std::collections::HashMap;
 
     let query = &query.unwrap_or("");
@@ -72,43 +86,41 @@ fn parse_query(query: Option<&str>) -> FutureResult<String, Error> {
         .filter(|s| !s.is_empty())
     {
         Some(value) => futures::future::ok(value),
-        // TODO: Throw error
-        None => futures::future::ok(String::from("error")),
+        None => futures::future::err(ServiceError::NoInput),
     }
 }
 
-fn parse_body(body: Body) -> FutureResult<String, Error> {
-    if body.content_length().unwrap_or(0) == 0 {
-        // TODO: Throw error
-        futures::future::ok(String::from("empty"))
-    } else {
+fn parse_body(body: Body) -> FutureResult<String, ServiceError> {
+    match body.content_length().unwrap_or(0) {
+        0 => futures::future::err(ServiceError::NoInput),
         // TODO: Extract body string value
-        futures::future::ok(String::from("que"))
+        _ => {
+            futures::future::ok(String::from("cc"))
+        },
     }
 }
 
-fn search_results(query: String) -> FutureResult<Body, Error> {
+fn search_results(query: String) -> FutureResult<Body, ServiceError> {
     // TODO: Parse query into searches
     futures::future::ok(Body::from(format!("Results for {:?}", query)))
 }
 
 // TODO: Find results function
 
-fn search_response(body: Result<Body, Error>) -> FutureResult<Response<Body>, Error> {
+fn search_response(body: Result<Body, ServiceError>) -> FutureResult<Response<Body>, ServiceError> {
     match body {
         Ok(body) => futures::future::ok(Response::new(body)),
-        // TODO: Show empty results
-        Err(err) => futures::future::ok(Response::new(Body::from("OK"))),
+        Err(err) => futures::future::ok(Response::new(vod_response_body())),
     }
 }
 
 impl NewService for SearchService {
     type ReqBody = Body;
     type ResBody = Body;
-    type Error = Error;
+    type Error = ServiceError;
     type Service = SearchService;
     type Future = Box<Future<Item = Self::Service, Error = Self::Error> + Send>;
-    type InitError = Error;
+    type InitError = ServiceError;
 
     fn new_service(&self) -> Self::Future {
         Box::new(futures::future::ok(SearchService))
@@ -118,7 +130,7 @@ impl NewService for SearchService {
 impl Service for SearchService {
     type ReqBody = Body;
     type ResBody = Body;
-    type Error = Error;
+    type Error = ServiceError;
     type Future = Box<Future<Item = Response<Self::ResBody>, Error = Self::Error> + Send>;
 
     fn call(&mut self, request: Request<Self::ReqBody>) -> Self::Future {
